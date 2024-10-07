82738cb01af753d18a6c8133c2d4cdd8b4182308
majabojdys <majabojdys98@gmail.com>
2024-10-07 11:05:06 +0200

    add index on email in user

--- src/main/java/com/maja/orderService/users/repositories/User.java
+++ src/main/java/com/maja/orderService/users/repositories/User.java
@@ -1,22 +1,13 @@
 package com.maja.orderService.users.repositories;
 
 import com.maja.orderService.orders.repositories.Order;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.util.List;
 import java.util.Objects;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users", indexes = @Index(columnList = "email"))
 public class User {
 
     @Id
